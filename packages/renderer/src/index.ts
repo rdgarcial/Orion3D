cc9f7ff9ac9a047a6d3e926f1e7480893d1757a2
Rubén Darío García López <rdgarcial@gmail.com>
2026-07-20 22:35:59 -0600

    003 feat(renderer): implement renderer abstraction layer

---
+++ packages/renderer/src/index.ts
@@ -0,0 +1,6 @@
+export * from "./core/Renderer";
+export * from "./adapters/RendererAdapter";
+export * from "./adapters/ThreeRendererAdapter";
+export * from "./factory/RendererFactory";
+export * from "./context/RenderContext";
+export * from "./loop/RenderLoop";
