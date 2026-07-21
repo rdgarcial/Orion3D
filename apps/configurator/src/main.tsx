2548d93fc0adaf48ccfbed3e00ac6149f257af18
Rubén Darío García López <rdgarcial@gmail.com>
2026-07-21 00:51:23 -0600

    feat(viewer): create professional Orion viewer

--- apps/configurator/src/main.tsx
+++ apps/configurator/src/main.tsx
@@ -1,24 +1,40 @@
 import React from "react";
-import {createRoot} from "react-dom/client";
-import {Canvas} from "@react-three/fiber";
+import { createRoot } from "react-dom/client";
+import { Canvas } from "@react-three/fiber";
+import { Grid, OrbitControls, Stats } from "@react-three/drei";
+import "./index.css";
 
-function Cube(){
-  return (
-    <mesh>
-      <boxGeometry args={[1,1,1]}/>
-      <meshStandardMaterial color="orange"/>
-    </mesh>
-  );
+function Scene() {
+    return (
+        <>
+            <ambientLight intensity={1.5} />
+            <directionalLight position={[5, 8, 5]} intensity={3} />
+
+            <Grid args={[20, 20]} />
+            <axesHelper args={[3]} />
+
+            <mesh castShadow position={[0, 0.5, 0]}>
+                <boxGeometry args={[1, 1, 1]} />
+                <meshStandardMaterial color="orange" />
+            </mesh>
+
+            <OrbitControls makeDefault />
+            <Stats />
+        </>
+    );
 }
 
-function App(){
-  return (
-    <Canvas camera={{position:[3,3,3]}}>
-      <ambientLight intensity={2}/>
-      <directionalLight position={[5,5,5]}/>
-      <Cube/>
-    </Canvas>
-  );
+function App() {
+    return (
+        <Canvas shadows camera={{ position: [4, 3, 6], fov: 45 }}>
+            <color attach="background" args={["#20232a"]} />
+            <Scene />
+        </Canvas>
+    );
 }
 
-createRoot(document.getElementById("root")!).render(<App/>);
+createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+);
